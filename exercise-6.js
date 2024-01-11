4378740553b5bf4a6f6c15fda1ffac28f3446a97
Jiran <paiveryhappy@gmail.com>
2024-01-12 03:28:15 +0700

    feat: use aggregate operator to query data

--- exercise-6.js
+++ exercise-6.js
@@ -1,1 +1,15 @@
 // Paste the complete MQL here
+db.pizzaOrders
+  .aggregate([
+    {
+      $group: {
+        _id: {
+          year: { $year: { $toDate: "$created_at" } },
+          month: { $month: { $toDate: "$created_at" } },
+        },
+        total_price_by_month: { $sum: "$total_price" },
+      },
+    },
+    { $match: { "_id.month": 7, "_id.year": 2021 } },
+  ])
+  .sort({ _id: -1 });
